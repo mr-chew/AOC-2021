9ceba0785ead1ee224723dd22c48e77e3eb0823a
chew <mr.krabs.6868@protonmail.com>
2021-12-03 10:01:34 +0800

    Initial commit

---
+++ Day01A.py
@@ -0,0 +1,14 @@
+report = []
+
+   
+def size():
+    return len(report)
+
+def empty():
+    return True if len(report) == 0 else False
+
+# get size of list
+# loop
+# 
+
+report = [199, 200, 208, 210, 200, 207,	240, 269, 260, 263]
